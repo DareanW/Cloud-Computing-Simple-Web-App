5ce50f534b2f00cd490fca943749375fd7e06019
Darean A Wilde <darean.wilde@my.utsa.edu>
2023-10-28 11:49:18 -0500

    Add project files.

---
+++ Cloud Computing Simple Web App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cloud_Computing_Simple_Web_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
