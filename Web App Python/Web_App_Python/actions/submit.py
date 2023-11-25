182e54b98f07cafb4aaf717c0f8a35911b6f7179
Darean A Wilde <darean.wilde@my.utsa.edu>
2023-11-25 09:35:13 -0600

    Got a successful connection to the database!

--- Web App Python/Web_App_Python/actions/submit.py
+++ Web App Python/Web_App_Python/actions/submit.py
@@ -1,5 +1,14 @@
 from flask import render_template, request
 
+from ..database.database import collection
+from pprint import pprint
+
+def print_all_documents():
+    documents = collection.find()
+    for document in documents:
+        pprint(document)
+
+
 def handle_submit():
     # Get the values from the form
     month = int(float(request.form.get('Month')))
@@ -8,4 +17,5 @@ def handle_submit():
     carrier = request.form.get('carrier')
     type = request.form.get('type')
     scheduled = int(float(request.form.get('Scheduled')))
+    print_all_documents()
     return render_template('success.html', month=month, usg_apt=usg_apt, fg_apt=fg_apt, carrier=carrier, type=type, scheduled=scheduled)
